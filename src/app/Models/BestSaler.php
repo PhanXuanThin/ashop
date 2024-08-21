26c1d30930a7af90fa4e3f6b248147cf7429a827
PhanXuanThin <xuanthin1999py@gmail.com>
2024-08-21 20:16:37 +0700

    Add Coupon.php file

---
+++ src/app/Models/BestSaler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BestSaler extends Model
+{
+    use HasFactory;
+
+    // Define the table name
+    protected $table = 'best_seller';
+}
